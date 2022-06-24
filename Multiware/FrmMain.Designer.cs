9889e74db7dec923d40773d5d7c539fbc1ca1c4b
Henrico Pelozato Maeda <henrico.maeda@gmail.com>
2022-06-24 01:59:55 -0300

    Desenvolvendo uma base com imagem

--- Multiware/FrmMain.Designer.cs
+++ Multiware/FrmMain.Designer.cs
@@ -28,21 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.picImage = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.btnChangeImage = new System.Windows.Forms.Button();
+            this.pnlMultiware = new System.Windows.Forms.Panel();
+            this.lblLine = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+            this.pnlMultiware.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // picImage
+            // 
+            this.picImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.picImage.Location = new System.Drawing.Point(32, 22);
+            this.picImage.Name = "picImage";
+            this.picImage.Size = new System.Drawing.Size(185, 164);
+            this.picImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picImage.TabIndex = 1;
+            this.picImage.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.Tan;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Courier New", 15F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(32, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.Tan;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Courier New", 15F);
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(32, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.Color.Tan;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Courier New", 15F);
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(32, 367);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(185, 43);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.BackColor = System.Drawing.Color.Tan;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Courier New", 15F);
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(32, 422);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(185, 43);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.BackColor = System.Drawing.Color.Tan;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Font = new System.Drawing.Font("Courier New", 15F);
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button5.Location = new System.Drawing.Point(32, 477);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(185, 43);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button6.BackColor = System.Drawing.Color.Tan;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Font = new System.Drawing.Font("Courier New", 15F);
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button6.Location = new System.Drawing.Point(32, 532);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(185, 43);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // btnChangeImage
+            // 
+            this.btnChangeImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChangeImage.BackColor = System.Drawing.Color.Tan;
+            this.btnChangeImage.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnChangeImage.Font = new System.Drawing.Font("Courier New", 10F);
+            this.btnChangeImage.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnChangeImage.Location = new System.Drawing.Point(32, 192);
+            this.btnChangeImage.Name = "btnChangeImage";
+            this.btnChangeImage.Size = new System.Drawing.Size(185, 30);
+            this.btnChangeImage.TabIndex = 6;
+            this.btnChangeImage.Text = "ATUALIZAR IMAGEM";
+            this.btnChangeImage.UseVisualStyleBackColor = false;
+            this.btnChangeImage.Click += new System.EventHandler(this.BtnChangeImage_Click);
+            // 
+            // pnlMultiware
+            // 
+            this.pnlMultiware.BackColor = System.Drawing.Color.LightGray;
+            this.pnlMultiware.Controls.Add(this.button6);
+            this.pnlMultiware.Controls.Add(this.picImage);
+            this.pnlMultiware.Controls.Add(this.button4);
+            this.pnlMultiware.Controls.Add(this.button2);
+            this.pnlMultiware.Controls.Add(this.button3);
+            this.pnlMultiware.Controls.Add(this.button5);
+            this.pnlMultiware.Controls.Add(this.button1);
+            this.pnlMultiware.Controls.Add(this.btnChangeImage);
+            this.pnlMultiware.Controls.Add(this.lblLine);
+            this.pnlMultiware.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlMultiware.Location = new System.Drawing.Point(0, 0);
+            this.pnlMultiware.Name = "pnlMultiware";
+            this.pnlMultiware.Size = new System.Drawing.Size(249, 597);
+            this.pnlMultiware.TabIndex = 7;
+            // 
+            // lblLine
+            // 
+            this.lblLine.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblLine.BackColor = System.Drawing.Color.Transparent;
+            this.lblLine.Font = new System.Drawing.Font("Arial", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLine.ForeColor = System.Drawing.Color.Orange;
+            this.lblLine.Location = new System.Drawing.Point(8, 250);
+            this.lblLine.Name = "lblLine";
+            this.lblLine.Size = new System.Drawing.Size(23, 58);
+            this.lblLine.TabIndex = 8;
+            this.lblLine.Text = "|";
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(870, 597);
+            this.Controls.Add(this.pnlMultiware);
             this.Name = "FrmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Multiware";
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            this.pnlMultiware.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picImage;
+        private System.Windows.Forms.Button btnChangeImage;
+        private System.Windows.Forms.Panel pnlMultiware;
+        private System.Windows.Forms.Label lblLine;
     }
 }
 
--- Multiware/FrmMain.cs
+++ Multiware/FrmMain.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Multiware
 {
@@ -15,6 +17,132 @@ namespace Multiware
         public FrmMain()
         {
             InitializeComponent();
+            Select();
+
+            string directory = @"C:\Multiware";
+            string file = @"C:\Multiware\icon.png";
+            string temp = @"C:\Multiware\icon_temp.png";
+
+            if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+            else if (File.Exists(temp))
+            {
+                if (File.Exists(temp)) File.Delete(file);
+                File.Copy(temp, file);
+                File.Delete(temp);
+                picImage.Image = Image.FromFile(file);
+            }
+
+            if (File.Exists(file)) picImage.Image = Image.FromFile(file);
+        }
+
+        private void BtnChangeImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+            ofd.Filter = "Arquivo de imagem|*.JPG; *.JPEG; *.PNG|" + "Todos os arquivos (*.*)|*.*";
+            ofd.Title = "Procurar imagem para atualizar";
+
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                string title = "";
+                MessageBoxIcon icon = MessageBoxIcon.Error;
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+
+                try
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(ofd.FileName);
+                    string fileExtension = Path.GetExtension(ofd.FileName);
+                    string[] fileExtensions = { ".jpg", ".jpeg", ".png" };
+
+                    if (!Array.Exists(fileExtensions, element => element == fileExtension)) MessageBox.Show("Nome do arquivo: " + fileName + "\nFormato de arquivo: " + fileExtension + "\n\nO formato de arquivo deve ser JPG, JPEG ou PNG.", title, buttons, icon);
+                    else
+                    {
+                        string file = @"C:\Multiware\icon_temp.png";
+                        if (File.Exists(file)) File.Delete(file);
+                        File.Copy(@ofd.FileName, file);
+
+                        dr = MessageBox.Show("Imagem atualizada com sucesso!" + "\nPara visualizá-la, re-abra o mesmo.\n\nAperte em SIM, para reinicia-lo.", title, MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                        if (dr == DialogResult.Yes) Application.Restart();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("ERRO: " + ex.Message, title, buttons, icon);
+                }
+            }
+        }
+
+        private void Animation(Button b)
+        {
+            int x = lblLine.Location.X;
+            int y = lblLine.Location.Y;
+            int y_02 = b.Location.Y;
+
+            if (b.Location.Y > y)
+            {
+                int y_03 = y_02 - y;
+                Task.Run(() =>
+                {
+                    for (int i = 0; i <= (y_03 - 7); i++)
+                    {
+                        Invoke((MethodInvoker)delegate
+                        {
+                            lblLine.Location = new Point(x, y++);
+                        });
+                    }
+                });
+            }
+            else
+            {
+                int y_03 = y - y_02;
+                Task.Run(() =>
+                {
+                    for (int i = (y_03 + 7); i >= 0; i--)
+                    {
+                        Invoke((MethodInvoker)delegate
+                        {
+                            lblLine.Location = new Point(x, y--);
+                        });
+                    }
+                });
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Animation(button1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Animation(button2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Animation(button3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Animation(button4);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Animation(button5);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Animation(button6);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            Application.ExitThread();
         }
     }
 }
